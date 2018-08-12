d8f8e8e3ee90b9b00394eb0ab6dca043646e3faf
casc1990 <962549206@qq.com>
2018-08-13 00:50:10 +0800

    Initial commit

---
+++ apps/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#Author:pengbo
+__date__ = '2018/8/12 23:01'
---
+++ apps/sre_redis/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SreRedisConfig(AppConfig):
+    name = 'apps.sre_redis'
